3af5a287d4416261a45a19f2d7d56331914ad4ec
lursmana1 <lursmana921@gmail.com>
2026-02-12 23:03:50 +0400

    change structure of the exam page

---
+++ src/app/components/ExamFooter/ExamFooter.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import leftSide from "../../../../public/png/left.png";
+import rightSide from "../../../../public/png/right.png";
+
+type ExamFooterProps = {
+  questions: {
+    key: string;
+    text: string | null;
+  }[];
+  showPrevious: () => void;
+  showNext: () => void;
+  selectAnswer: (key: string) => void;
+};
+
+const ExamFooter = (props: ExamFooterProps) => {
+  return (
+    <div className="flex justify-between items-center bg-red-50 h-[10vh] w-full px-4">
+      {/* Back */}
+      <button
+        onClick={props.showPrevious}
+        className="p-2 flex justify-center items-center bg-gray-200 hover:bg-gray-300 rounded-md"
+      >
+        <Image src={leftSide} alt={""} width={32} height={32} />
+      </button>
+
+      {/* Question Numbers */}
+      <div className="flex gap-2 overflow-x-auto">
+        {props.questions.map((question) => (
+          <div
+            key={question.key}
+            onClick={() => props.selectAnswer(question.key)}
+            className="
+                flex items-center justify-center
+                bg-gray-100 text-black
+                border border-gray-400
+                w-12 h-12
+                rounded-md
+                font-bold text-lg
+                cursor-pointer
+                hover:bg-gray-200
+                transition
+              "
+          >
+            {question.key}
+          </div>
+        ))}
+      </div>
+
+      <button
+        onClick={props.showNext}
+        className="p-2 flex justify-center items-center bg-gray-200 hover:bg-gray-300 rounded-md"
+      >
+        <Image src={rightSide} width={32} height={32} alt="zd" />
+      </button>
+    </div>
+  );
+};
+
+export default ExamFooter;
--- src/app/components/QuizButton/QuizButton.tsx
+++ src/app/components/QuizButton/QuizButton.tsx
@@ -1,12 +1,9 @@
-import { ReactNode } from "react";
-
 type QuizButtonProps = {
   selectAnswer: (value: string) => void;
   answerKey: string;
   answerText: string;
   selectedAnswer: string;
   correctAnswer: string;
-  children: ReactNode;
 };
 
 const QuizButton = ({
@@ -20,29 +17,27 @@ const QuizButton = ({
   const isThisSelected = selectedAnswer === answerKey;
   const isThisCorrect = answerKey === correctAnswer;
 
-  const isDisabled = hasSelected;
   const base =
-    "w-full text-left flex items-center p-2 rounded border transition-colors h-20";
+    "w-full h-full text-left font-georgian text-white flex items-start rounded border transition-colors p-3 leading-snug break-words";
 
   const stateClass = hasSelected
     ? isThisCorrect
-      ? "bg-green-100 border-green-500"
+      ? "bg-[#05c300c9] border-[#c3e6cb;] text-black"
       : isThisSelected
-        ? "bg-red-100 border-red-500"
-        : "border-gray-300"
+        ? "bg-[#ff3346a8] border-[#f5c6cb] text-black"
+        : "border-gray-300 opacity-70"
     : "border-gray-300 hover:border-blue-300 cursor-pointer";
-
-  const cursorClass = hasSelected ? "cursor-default" : "";
 
   return (
     <button
       type="button"
       onClick={() => selectAnswer(answerKey)}
-      disabled={isDisabled}
-      className={`${base} ${stateClass} ${cursorClass}`}
+      disabled={hasSelected}
+      className={`${base} ${stateClass}`}
     >
       {answerText}
     </button>
   );
 };
+
 export default QuizButton;
